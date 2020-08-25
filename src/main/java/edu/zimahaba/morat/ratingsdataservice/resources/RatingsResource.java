a1019b423eac3950d7e4d5a9095315370eb40937
Paulo <pmaquitorres@gmail.com>
2020-08-25 00:56:09 -0300

    first commit

---
+++ src/main/java/edu/zimahaba/morat/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,17 @@
+package edu.zimahaba.morat.ratingsdataservice.resources;
+
+import edu.zimahaba.morat.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @GetMapping("")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
---
+++ src/test/java/edu/zimahaba/morat/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zimahaba.morat.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
